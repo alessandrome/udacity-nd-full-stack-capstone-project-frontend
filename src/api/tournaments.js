f8f10b9bf873611090296b3ea7383714220f1af2
alessandrome <mesti.alessandro.git@gmail.com>
2020-03-02 21:09:40 +0100

    Making API file for tournament andpoints

---
+++ src/api/tournaments.js
@@ -0,0 +1,44 @@
+import axios from 'axios';
+const BASE_URL = process.env.VUE_APP_BASE_API_URL;
+
+const requests = {
+    /**
+     * Get API request to get match list from API endpoint
+     * @param data
+     * @param filters
+     * @param page
+     * @param perPage
+     * @returns {Promise<AxiosResponse<T>>}
+     */
+    getTournaments(data, filters, page, perPage) {
+        return axios.
+            get(`${BASE_URL}/tournaments`, {props: Object.assign({}, data, filters, {page, perPage})});
+    },
+    getTournament(uuid) {
+        return axios.
+            get(`${BASE_URL}/tournaments/${uuid}`);
+    },
+    createTournament(data) {
+        return axios.
+            post(`${BASE_URL}/tournaments`, Object.assign({}, data));
+    },
+    patchTournament(tournamentId, action, data) {
+        data = data || {};
+        return axios.
+            patch(`${BASE_URL}/tournaments/${tournamentId}`, Object.assign({}, data, {action}));
+    },
+    /**
+     *
+     * @param matchId
+     * @param joined {1|0}
+     */
+    getTournamentUsers(tournamentId, joined = 1) {
+        return axios.
+            get(`${BASE_URL}/tournaments/${tournamentId}/users`, {params: Object.assign({}, {joined})});
+    }
+};
+
+export default {
+    namespaced: true,
+    requests
+}
